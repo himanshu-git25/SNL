1f58e1c741daabfd4a2496e7ed5b14a514e06304
Himanshu Choudhary <himanshu03417@gmail.com>
2025-07-29 23:03:11 +0530

    serviceWorker.js

---
+++ serviceWorker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "snl-ide-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/interpreter.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
+
+// Install event
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch event
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).catch(() => {
+          return caches.match("/offline.html");
+        })
+      );
+    })
+  );
+});
